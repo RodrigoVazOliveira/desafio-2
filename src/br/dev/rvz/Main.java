02dfac923c5fe7074e2fc27196007b2b8e5a1db0
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-02 16:07:53 -0300

    implementado a exclusão de contato

--- src/br/dev/rvz/Main.java
+++ src/br/dev/rvz/Main.java
@@ -13,16 +13,20 @@ public class Main {
         System.out.println("Bem vindo ao sistema de agenda!");
 
         while (continuar) {
-            System.out.println("1. Deseja adicionar novo contato.\n2. Sair do sistema.");
+            System.out.println("1. Deseja adicionar novo contato.\n2. Excluir contato por e-mail\n3. Sair do sistema.");
             opcao = Integer.parseInt(in.nextLine());
 
             if (opcao == 1) {
                 System.out.println("Digite o nome, telefone e email: ");
                 String name = in.nextLine();
                 String telefone = in.nextLine();
-                String email  = in.nextLine();
+                String email = in.nextLine();
                 agenda.put(email, Arrays.asList(name, telefone));
             } else if (opcao == 2) {
+                System.out.println("Digite o e-mail do contato: ");
+                String email = in.nextLine();
+                agenda = removerContato(agenda, email);
+            } else if (opcao == 3) {
                 continuar = false;
             }
         }
@@ -34,5 +38,11 @@ public class Main {
             List<String> dados =  (List) contato.getValue();
             System.out.println("Nome: " + dados.get(0) + "\nTelefone: " + dados.get(1) + "\nE-Mail: " + contato.getKey());
         }
+    }
+
+    private static Map<String, List<String>> removerContato(Map<String, List<String>> agenda, String email) {
+        List<String> contato = agenda.remove(email);
+        System.out.println("foi removido o contato " + contato.get(0) + " com telefone " + contato.get(1));
+        return agenda;
     }
 }
